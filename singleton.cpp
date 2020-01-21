f17ea6371879c908449583f558ee00c92bef3cc5
IpankajI <kmrpnk@gmail.com>
2020-01-21 21:32:08 +0530

    Singleton by call_once and lock_gaurd

--- singleton.cpp
+++ singleton.cpp
@@ -1,37 +1,102 @@
 //A singelton class in C++ (thread safe)
 
 #include <iostream>
+#include <future>
+#include <cassert>
+
 using namespace std;
 
-class Config{
+/*****************************************************************************************************/
+
+class MayerSingleton{
     private:
+    MayerSingleton() = default;
+    ~MayerSingleton() = default;
+    MayerSingleton(const MayerSingleton&)=delete;
+    MayerSingleton& operator = (const MayerSingleton&)=delete;
 
-    Config() = default;
-    ~Config() = default;
-    Config(const Config&)=delete;
-    Config& operator = (const Config&)=delete;
-
-    public: //config variables
-    string serverIP;
-    int serverPort;
-
-    public: // get and object of Config type
-    static Config& getInstance(){
-        // Mayer Singleton (static variable with block scope)
-        static Config config;
-        return config; 
+    public:
+    static MayerSingleton* getInstance(){
+        //static variable with block scope
+        static MayerSingleton* singleton=new MayerSingleton();
+        return singleton; 
     }
 };
 
-void useConfig(Config& cfg){
-    cout<<"Server IP: "<<cfg.serverIP<<endl;
-    cout<<"Server Port: "<<cfg.serverPort<<endl;
-}
+
+/*****************************************************************************************************/
+
+class Singleton_by_call_once{
+    private:
+
+    Singleton_by_call_once() = default;
+    ~Singleton_by_call_once() = default;
+    Singleton_by_call_once(const Singleton_by_call_once&)=delete;
+    Singleton_by_call_once& operator = (const Singleton_by_call_once&)=delete;
+
+
+    static once_flag initOnceFlag;
+    static Singleton_by_call_once* singleton;
+
+    static void initConfig(){
+        singleton=new Singleton_by_call_once();
+    }
+
+    public:
+    static Singleton_by_call_once* getInstance(){
+        call_once(initOnceFlag, initConfig);
+        return singleton;
+    }
+
+};
+once_flag Singleton_by_call_once::initOnceFlag;
+Singleton_by_call_once* Singleton_by_call_once::singleton=NULL;
+
+/*****************************************************************************************************/
+
+class SingletonByLock{
+    private:
+
+    SingletonByLock() = default;
+    ~SingletonByLock() = default;
+    SingletonByLock(const SingletonByLock&)=delete;
+    SingletonByLock& operator = (const SingletonByLock&)=delete;
+
+    static SingletonByLock* singleton;
+    static mutex mtx;
+    public:
+    static SingletonByLock* getInstance(){
+        lock_guard<mutex> lckGuard(mtx);
+        if(singleton==NULL){
+            singleton=new SingletonByLock();
+        }
+        return singleton;
+    }
+
+};
+
+SingletonByLock* SingletonByLock::singleton=NULL;
+mutex SingletonByLock::mtx;
+
+/*****************************************************************************************************/
 
 int main() {
-    Config& config=Config::getInstance();
-    config.serverIP="27.0.0.1";
-    config.serverPort=8997;
 
-    useConfig(config);
+    MayerSingleton* configMayerSingleton1=MayerSingleton::getInstance();
+    MayerSingleton* configMayerSingleton2=MayerSingleton::getInstance();
+    assert(configMayerSingleton1==configMayerSingleton2 && configMayerSingleton1!=NULL && configMayerSingleton2!=NULL);
+
+    Singleton_by_call_once* configSingleton_by_call_once1=Singleton_by_call_once::getInstance();
+    Singleton_by_call_once* configSingleton_by_call_once2=Singleton_by_call_once::getInstance();
+    assert(configSingleton_by_call_once1==configSingleton_by_call_once2  && configSingleton_by_call_once1!=NULL && configSingleton_by_call_once2!=NULL);
+
+    const int N=100000;
+    SingletonByLock* a[N];
+    for(int i=0;i<N;++i){
+        a[i]=SingletonByLock::getInstance();
+    }
+    for(int i=1;i<N;++i){
+        assert(a[i-1]==a[i] && a[i]!=NULL);
+    }
+
 }
