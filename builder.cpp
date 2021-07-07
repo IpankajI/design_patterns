55e6e538e3da353dec7e7c0d5f73a6d1c08f18a2
Pankaj Kumar <kmrpnk@gmail.com>
2021-07-07 21:58:49 +0530

    without pointer

--- builder.cpp
+++ builder.cpp
@@ -1,88 +1,65 @@
-#include<iostream>
-#include<unordered_map>
-#include<mutex>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-class CHttpRequest{
-    string url;
-    string method;
-    unordered_map<string, string> params;
-    
-public:
 
-    class CHttpRequestBuilder{
-        string url;
-        string method;
-        unordered_map<string, string> params;
+class CHTTPRequest{
+
     public:
-        CHttpRequestBuilder* setUrl(string url){
-            this->url=url;
-            return this;
-        }
-        CHttpRequestBuilder* setMethod(string method){
-            this->method=method;
-            return this;
-        }
-        CHttpRequestBuilder* addParam(string key, string value){
-            this->params[key]=value;
-            return this;
-        }
-        CHttpRequest* build(CHttpRequest* httpRequest){
-            httpRequest->url=url;
-            httpRequest->method=method;
-            httpRequest->params=params;
-            return httpRequest;
-        }
+    class CHTTPRequestBuilder{
+        public:
+            string method;
+            string url;
+            map<string, string> params;
+            CHTTPRequestBuilder setURL(string url){
+                this->url=url;
+                return *this;
+            }
+            CHTTPRequestBuilder setMethod(string method){
+                this->method=method;
+                return *this;
+            }
+            CHTTPRequestBuilder setParam(string key, string value){
+                this->params[key]=value;
+                return *this;
+            }
+
+            CHTTPRequest build(){
+                return CHTTPRequest(this);
+            }
+
     };
 
-    
-    static mutex mtx;
-    CHttpRequestBuilder* HttpRequestBuilder(){
-        lock_guard<mutex> lck(mtx);
-        if(httpRequestBuilder==NULL){
-            httpRequestBuilder= new CHttpRequestBuilder();
-        }
-        return httpRequestBuilder;
+    private:
+    string method;
+    string url;
+    map<string, string> params;
+
+    public:
+    CHTTPRequest(CHTTPRequestBuilder* builder){
+        url=builder->url;
+        method=builder->method;
+        params=builder->params;
     }
 
-    bool Do(){  
-        
-        cout<<"url: "<<url<<endl;
-        cout<<"method: "<<method<<endl;
-
-        for(auto it=params.begin(); it!=params.end(); ++it){
-            cout<<it->first<<": "<<it->second<<endl;
-        }
-        cout<<"Sending HttpRequest..."<<endl;
-        return true;
+    static CHTTPRequestBuilder httpRequestBuilder(){
+        return CHTTPRequestBuilder();
     }
 
-    CHttpRequest(){
-        httpRequestBuilder=NULL;
-    }
-    ~CHttpRequest(){
-        if(httpRequestBuilder){
-            delete httpRequestBuilder;
-            httpRequestBuilder=NULL;
+    void Do(){
+        cout<<"making request with method: "<<method<<", url: "<<url<<endl;
+        cout<<"params-"<<endl;
+        for(auto e: params){
+            cout<<e.first<<" "<<e.second<<endl;
         }
     }
-private:
-    CHttpRequestBuilder* httpRequestBuilder;
+
 };
 
-mutex CHttpRequest::mtx;
 
 int main(){
+    
+    CHTTPRequest req=CHTTPRequest::httpRequestBuilder().setMethod("post").setURL("example.com").setParam("key", "value").build();
+    req.Do();
 
-    CHttpRequest* httpRequest=new CHttpRequest();
-    httpRequest->HttpRequestBuilder()
-        ->setUrl("example.com")
-        ->setMethod("get")
-        ->addParam("page", "1")
-        ->addParam("user", "pankaj kumar")
-        ->build(httpRequest)
-        ->Do();
-
-    delete httpRequest;
 }
